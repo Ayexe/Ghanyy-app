7c5b43b921b8b6772f079b313c944a8b6b36880f
Ayexe <yudhoyonoal@gmail.com>
2025-04-23 15:18:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,16 @@ import streamlit as st
 
 st.title("🎈 Gacor!")
 st.write("Situs paling gacor!")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A nadhif")
+    st.image
+        
+    
+     
