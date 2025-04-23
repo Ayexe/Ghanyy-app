5c5c8ac9a0c63efe370df6acecba4e89701fa696
Ayexe <yudhoyonoal@gmail.com>
2025-04-23 17:52:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,13 @@
 import streamlit as st
 
-st.title("🎈 Gacor!")
-st.write("Situs paling gacor!")
+st.title("🎈 Willard Al Ghany Yudhoyono")
+st.write("Ada kucing nih")
 
 col1, col2, col3 = st.columns(3)
 
 with col1:
     st.header("A cat")
     st.image("https://static.streamlit.io/examples/cat.jpg")
-
-with col2:
-    st.header("A nadhif")
-    st.image
-        
+    
     
      
