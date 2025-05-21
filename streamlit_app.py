9b0b2f32f5f281a1f9d316b99d442b36f5f3bc33
Ayexe <yudhoyonoal@gmail.com>
2025-05-21 14:51:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,7 @@ import streamlit as st
 
 st.title("Indojaw!")
 st.write("!INDOMARET JAWA PRIDE!")
-
-st.image("IMG_20250520_145036.jpg", width=200)
+st.image("IMG_20250520_145036.jpg", width=2000)
     
     
      
