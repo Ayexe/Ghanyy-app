2658b37882ee94c66f8ca053e083f037be8f6d29
Ayexe <yudhoyonoal@gmail.com>
2025-05-21 15:06:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,17 @@
 import streamlit as st
 
-st.title("RADIT PINTER")
+st.title("RADITMAXWIN999")
 st.write("LIAT INI GANTENG BGT DIA")
 st.image("IMG_20250520_145036.jpg", width=2000)
 
-st.title("Aplikasi Radit pinter ngitung")
-st.header("Radit bisa ngitung, coba aja")
-angka = st.number_input ("coba tulis angka apa aja, si didits mah jago:", value=0, step=1)
+st.title("MAXWIN DI JAMIN GACOR!")
+st.header("RADIT INGIN TES KEBERUNTUNGAN ANDA")
+angka = st.number_input ("MASUKAN ANGKA BERUNTUNG ANDA!:", value=0, step=1)
 
 if (angka % 2) == 0:
-    st.write(f"{angka} Kata Radit itu adalah Bilangan genap")
+    st.write(f"{angka} RADIT BERSABDA, ANDA KURANG BERUNTUNG")
 else:
-    st.write(f"{angka} Kata Radit itu adalah Bilangan ganjil")
+    st.write(f"{angka} RADIT BERSABDA, ANDAAA MAXWINNNN GACORRR")
     
     
      
