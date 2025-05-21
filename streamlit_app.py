474dff844cf43446f78213cf396f7854e6d333a6
Ayexe <yudhoyonoal@gmail.com>
2025-05-21 14:56:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
-st.title("INDOJAW!")
-st.write("!INDOMARET JAWA PRIDE!")
+st.title("RADIT PINTER")
+st.write("LIAT INI GANTENG BGT DIA")
 st.image("IMG_20250520_145036.jpg", width=2000)
 
 st.title("Aplikasi Radit pinter ngitung")
 st.header("Radit bisa ngitung, coba aja")
-angka = st.number_input ("tulis sebuah angka:", value=0, step=1)
+angka = st.number_input ("coba tulis angka apa aja, si didits mah jago:", value=0, step=1)
 
 if (angka % 2) == 0:
     st.write(f"{angka} adalah Bilangan genap")
