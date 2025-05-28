a7d0152fddd81f8ef69f92e508f078928a0b2f6b
Ayexe <yudhoyonoal@gmail.com>
2025-05-28 14:20:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,33 +13,18 @@ if (angka % 2) == 0:
 else:
     st.write(f"{angka} RADIT MEMIKIR... ANDA MAXWINNNN GACORRR")
     
-st.title("Aplikasi Sederhana")
+st.header("Aplikasi menghitung Total Belanja")
 
-# Menggunakan layout kolom
-col1, col2 = st.columns(2)
+def hitung_total(harga, jumlah):
+return harga * jumlah
 
-with col1:
-    st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
-    angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
+harga_barang = st.number_input("Masukkan Harga Barang:", value=0, step=1000)
+jumlah_barang = st.number_input("Masukkan Jumlah Barang:", value=0, step=1)
+total_harga = hitung_total(harga_barang, jumlah_barang)
 
-    if (angka % 2) == 0:
-        st.write(f"{angka} adalah Bilangan Genap")
-    else:
-        st.write(f"{angka} adalah Bilangan Ganjil")
-
-with col2:
-    st.header("Aplikasi menghitung Total Belanja")
-
-    def hitung_total(harga, jumlah):
-        return harga * jumlah
-
-    harga_barang = st.number_input("Masukkan Harga Barang:", value=0, step=1000)
-    jumlah_barang = st.number_input("Masukkan Jumlah Barang:", value=0, step=1)
-    total_harga = hitung_total(harga_barang, jumlah_barang)
-
-    if total_harga > 100000:
-        total_harga_diskon = total_harga - 0.05 * total_harga
-        st.write(f"Total Harga (dengan diskon): Rp. {total_harga_diskon:,.0f}")
+if total_harga > 100000:
+total_harga_diskon = total_harga - 0.05 * total_harga
+st.write(f"Total Harga (dengan diskon): Rp. {total_harga_diskon:,.0f}")
     else:
         st.write(f"Total Harga: Rp. {total_harga:,.0f}")
 
